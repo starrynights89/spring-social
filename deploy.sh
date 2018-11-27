f22dd856d8aa6f4822e922dbeebee3c1dc9ffeb2
Alexander Hartson <alexander-hartson@live.com>
2018-11-26 19:28:33 -0600

    adding code changes

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cf push spring-social-config-server -p config-server/build/libs/config-server-0.0.1-SNAPSHOT.jar &
-cf push spring-social-eureka-server -p build/libs/spring-social-eureka-server-0.0.1-SNAPSHOT.jar &
+cf push spring-social-eureka-server -p eureka/build/libs/eureka-0.0.1-SNAPSHOT.jar &
 cf push spring-social-chat -p chat/build/libs/chat-0.0.1-SNAPSHOT.jar &
 cf push spring-social-comments -p comments/build/libs/comments-0.0.1-SNAPSHOT.jar &
 cf push spring-social-images -p images/build/libs/images-0.0.1-SNAPSHOT.jar &
